71c76ffe835cad294fde8248c66a92ca54707441
ReneLumiste <lumisterene@gmail.com>
2018-11-15 14:15:16 +0200

    css ja js

---
+++ genejs.js
@@ -0,0 +1,14 @@
+
+
+function pilt1() {
+	document.getElementById("busi").src="https://imagesvc.timeincapp.com/v3/fan/image?url=https://everythingbarca.com/wp-content/uploads/getty-images/2018/03/923843044-fc-barcelona-v-girona-la-liga-santander.jpg-850x560.jpg&c=sc&w=850&h=560";
+}
+// function pilt2() {
+//	document.getElementById("busi").src="https://images.performgroup.com/di/library/GOAL/d4/8b/sergio-busquets-barcelona-2018-19_f65inampe5jf19e26jp028gsh.jpg?t=-59994675&quality=90&w=1280";
+//}
+
+function pilt2() {
+	document.getElementById("busi").src="http://jalgpall.ee/images/players/05254CBDA711AD9F4B31DEE56B1B4668";
+}
+
+window.alert("Avarda oma m6ttemaalima ,<@<.")
